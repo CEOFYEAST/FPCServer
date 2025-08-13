f1c12024e3588cbd37d4e3a25ac7dd1615bc595d
CEOFYEAST <benton.diebold@me.com>
2025-08-13 14:49:10 -0500

    Add everything lol

---
+++ src/scripts/queryAccount.js
@@ -0,0 +1,13 @@
+const CredentialsCollectionName = process.env["CREDENTIALS_COLLECTION"]
+const DatabaseName = process.env["DATABASE"]
+
+async function queryAccount(app, username){
+    await app.ready()
+    let db = app.mongo.client.db(DatabaseName);
+    let coll = db.collection(CredentialsCollectionName);
+    const query = { username: username };
+    let result = await coll.findOne(query);
+    return result != null;
+}
+
+module.exports = queryAccount
