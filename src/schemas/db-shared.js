7b58c8cb0720f2a7b73b1bab7b6c1e6a11402d35
CEOFYEAST <benton.diebold@me.com>
2025-08-27 21:29:40 -0500

    Fix bug

--- src/schemas/db-shared.js
+++ src/schemas/db-shared.js
@@ -1,5 +1,23 @@
 const ProdChainObjectSchema = {
     timeUnit: "minute",
+    crafterConfig: {
+        "miner": "electric-mining-drill",
+        "smelter": "steel-furnace",
+        "assembler": "assembling-machine-1",
+        "fluid-assembler": "assembling-machine-2",
+        "chem": "chemical-plant",
+        "centrifuge": "centrifuge",
+        "oil-pump": "pumpjack",
+        "refinery": "oil-refinery",
+        "silo": "rocket-silo",
+        "reactor": "nuclear-reactor",
+        "water-pump": "offshore-pump",
+        "manual": ""
+    },
+    beltConfig: {
+        "belt-type": "transport-belt",
+        "pipe-type": "pipe"
+    },
     prodChain: {}
 }
 
